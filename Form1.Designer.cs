20b30468445d6cb7e9feb83a7466eaee85fe51d9
sunmantra <zcernyh14@gmail.com>
2021-03-27 13:23:03 +0700

    доработка засшифровки

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -59,6 +64,10 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox4);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pictureBox1);
@@ -67,28 +76,45 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(602, 291);
+            this.panel1.Size = new System.Drawing.Size(914, 291);
             this.panel1.TabIndex = 1;
             // 
-            // label1
+            // textBox3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(17, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 35);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "квадрат Полибия";
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(610, 194);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(280, 40);
+            this.textBox3.TabIndex = 7;
             // 
-            // pictureBox1
+            // label4
             // 
-            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.palibi;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 38);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(248, 237);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(604, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 35);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "исходный текст";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(610, 116);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(280, 37);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(291, 194);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(280, 40);
+            this.textBox2.TabIndex = 3;
             // 
             // label2
             // 
@@ -100,15 +126,15 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "исходный текст";
             // 
-            // label3
+            // pictureBox1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(285, 156);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(273, 35);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "зашифрованный текст";
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.palibi;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 237);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // textBox1
             // 
@@ -119,21 +145,59 @@
             this.textBox1.Size = new System.Drawing.Size(280, 37);
             this.textBox1.TabIndex = 2;
             // 
-            // textBox2
+            // label3
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(291, 194);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(280, 40);
-            this.textBox2.TabIndex = 3;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(285, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(273, 35);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "зашифрованный текст";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "квадрат Полибия";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(604, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(273, 35);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "зашифрованный текст";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 13F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(622, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(280, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "расшифровать";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(627, 365);
+            this.ClientSize = new System.Drawing.Size(938, 365);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -155,6 +219,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -49,5 +49,34 @@ namespace WindowsFormsApp1
             }
             textBox2.Text = new_message;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char[,] alf = { {'A', 'B', 'C','D','E'},
+                            {'F','G','H','I','K'},
+                            { 'L','M','N','O','P'},
+                            { 'Q','R','S','T','U'},
+                            { 'V','W','X','Y','Z'}
+                            };
+
+            string message = textBox4.Text;
+            string new_message = "";
+           
+            for (int k = 0; k < message.Length; k++){
+                for (int i = 0; i < alf.GetLongLength(0); i++)
+                    for (int j = 0; j < alf.GetLongLength(1); j++)
+                        if (Char.ToLower(alf[i, j]) == message[k] || Char.ToUpper(alf[i, j]) == message[k])
+                            if (i == 0)
+                            {
+                                new_message += Char.ToLower(alf[4, j]);
+                                break;
+                            }
+                            else {
+                                new_message += Char.ToLower(alf[i - 1, j]);
+                                break;
+                            }
+            }                 
+            textBox3.Text = new_message;
+        }
     }
 }
